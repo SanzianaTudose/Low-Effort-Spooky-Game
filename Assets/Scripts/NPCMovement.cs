031c7eec91e1871d85571878e0f91d66216c794e
SanzianaTudose <39738506+SanzianaTudose@users.noreply.github.com>
2021-11-12 20:48:15 +0100

    NPC movement done

--- Assets/Scripts/NPCMovement.cs
+++ Assets/Scripts/NPCMovement.cs
@@ -4,25 +4,45 @@ using UnityEngine;
 
 public class NPCMovement : MonoBehaviour
 {
-    [SerializeField] protected float moveSpeed = 1f;
-    [SerializeField] protected Vector2 decisionTimeRange = new Vector2(1f, 2f);
+    [SerializeField] private Vector2 moveSpeedRange = new Vector2(1.5f, 2f);
+    [SerializeField] private Vector2 decisionTimeRange = new Vector2(2f, 4f);
+    [SerializeField] private Vector2 waitTimeRange = new Vector2(1f, 2f);
+    [SerializeField] private float waitProbability = 0.2f;
 
-    private Vector3 curMoveDir;
+    private float moveSpeed;
     private float decisionTimeCount = 0;
+    
+    private Vector3 curMoveDir;
 
-    void Update() {
+    private void Start() {
+        moveSpeed = Random.Range(moveSpeedRange.x, moveSpeedRange.y);
+    }
+
+    private void Update() {
         // Move the object in the chosen direction at the set speed
         transform.position += curMoveDir * Time.deltaTime * moveSpeed;
 
         if (decisionTimeCount > 0) decisionTimeCount -= Time.deltaTime;
-        else {
-            decisionTimeCount = Random.Range(decisionTimeRange.x, decisionTimeRange.y);
-            GenerateMoveDirection();
-        }
+        else StartNewMovement();
     }
 
-    void GenerateMoveDirection() {
+    private void StartNewMovement() {
+        decisionTimeCount = Random.Range(decisionTimeRange.x, decisionTimeRange.y);
+
+        // Generate new movement direction
         curMoveDir = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f);
+        float isWaiting = Random.Range(0.0f, 1f);
+        if (isWaiting <= waitProbability) {
+            curMoveDir = Vector3.zero;
+            decisionTimeCount = Random.Range(waitTimeRange.x, waitTimeRange.y);
+        }
         curMoveDir.Normalize();
+
+        decisionTimeCount = Random.Range(decisionTimeRange.x, decisionTimeRange.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        // Move away when colliding with an object
+        curMoveDir = -curMoveDir;
     }
 }
