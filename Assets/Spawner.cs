c3fa1b4104a5da100dbfcc4cc8e97682087c06ed
stratosefty <stratosefty@gmail.com>
2021-11-13 21:39:23 +0100

    Added non spawnable sea

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -4,17 +4,67 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject NPCPrefab;
+    public GameObject BobPrefab;
+    private bool spawnedYet = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        while (!spawnedYet)
+        {
+            SpawnEnemyNPC();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void SpawnEnemyNPC()
+    {
+        if (!spawnedYet)
+        {
+            Vector2 randPosition = new Vector2(Random.Range(-30.0f, 8.8f), Random.Range(-19.0f, 6.0f));
+            GameObject npc = Instantiate(BobPrefab);
+            npc.transform.position = randPosition;
+
+            if (!InCollider())
+            {
+                spawnedYet = true;
+            }
+            else if (InCollider())
+            {
+                Destroy(npc);
+            }
+            //GameObject npc = Instantiate(BobPrefab);
+            //npc.transform.position = new Vector2(Random.Range(-30.0f, 8.8f), Random.Range(-19.0f, 6.0f));
+        }
+    }
+
+    private bool InCollider()
+    {
+        Vector2 position = new Vector2(BobPrefab.transform.position.x - 0.22f, BobPrefab.transform.position.y);
+        Vector2 direction = Vector2.down;
+        float distance = 0.6f;
+
+        //RaycastHit2D hit = Physics2D.Raycast(position, direction, distance);
+
+        Debug.DrawRay(position, direction, Color.green);
+        RaycastHit2D downRay = Physics2D.Raycast(position, Vector2.down, distance);
+        RaycastHit2D upRay = Physics2D.Raycast(position, Vector2.up, distance);
+        RaycastHit2D leftRay = Physics2D.Raycast(position, Vector2.left, distance);
+        RaycastHit2D rightRay = Physics2D.Raycast(position, Vector2.right, distance);
+
+        if (downRay.collider != null && upRay.collider != null && leftRay.collider != null && rightRay.collider != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
     }
 }
