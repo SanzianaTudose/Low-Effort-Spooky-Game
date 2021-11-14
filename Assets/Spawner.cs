ae8f615bcc88f77fb204f291027dd95a1daf9a14
stratosefty <stratosefty@gmail.com>
2021-11-14 13:02:26 +0100

    Random spawning after depossessing the NPC

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+
     public GameObject BobPrefab;
     private bool spawnedYet = false;
+    public LayerMask mask;
 
     // Start is called before the first frame update
     void Start()
     {
-        while (!spawnedYet)
+        while (spawnedYet == false)
         {
             SpawnEnemyNPC();
         }
@@ -30,12 +32,14 @@ public class Spawner : MonoBehaviour
             GameObject npc = Instantiate(BobPrefab);
             npc.transform.position = randPosition;
 
-            if (!InCollider())
+            if (!Physics2D.OverlapCircle(npc.transform.position, 2f))
             {
+                Debug.Log("Nothing Touches b");
                 spawnedYet = true;
             }
-            else if (InCollider())
+            else if (Physics2D.OverlapCircle(npc.transform.position, 2f))
             {
+                Debug.Log("Something touches");
                 Destroy(npc);
             }
             //GameObject npc = Instantiate(BobPrefab);
@@ -47,17 +51,18 @@ public class Spawner : MonoBehaviour
     {
         Vector2 position = new Vector2(BobPrefab.transform.position.x - 0.22f, BobPrefab.transform.position.y);
         Vector2 direction = Vector2.down;
-        float distance = 0.6f;
+        float distance = 20f;
 
         //RaycastHit2D hit = Physics2D.Raycast(position, direction, distance);
 
         Debug.DrawRay(position, direction, Color.green);
         RaycastHit2D downRay = Physics2D.Raycast(position, Vector2.down, distance);
+        Debug.DrawRay(position, direction, Color.red);
         RaycastHit2D upRay = Physics2D.Raycast(position, Vector2.up, distance);
         RaycastHit2D leftRay = Physics2D.Raycast(position, Vector2.left, distance);
         RaycastHit2D rightRay = Physics2D.Raycast(position, Vector2.right, distance);
 
-        if (downRay.collider != null && upRay.collider != null && leftRay.collider != null && rightRay.collider != null)
+        if (downRay.collider == null && upRay.collider == null && leftRay.collider == null && rightRay.collider == null)
         {
             return true;
         }
