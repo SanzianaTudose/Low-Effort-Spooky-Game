d73358b75b96bcee496e8d15780f1a6b2da37027
stratosefty <70846251+stratosefty@users.noreply.github.com>
2021-11-14 14:44:18 +0100

    NPC Spawner
    * Revert "Flower Click Mini-game (#3)"
    
    This reverts commit d8b5c5dff72f6a40f710f19fc3ece91fba18f501.
    
    * Added non spawnable sea
    
    * Random spawning after depossessing the NPC
    
    * Adjusted spawn location choosing
    
    * Added some comments
    
    * Completed spawning
    
    * Added URP renderer
    
    * Small changes
    
    Co-authored-by: SanzianaTudose <39738506+SanzianaTudose@users.noreply.github.com>

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -4,17 +4,45 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject NPCPrefab;
+
+    public GameObject[] Prefabs;
+    private bool[] spawnedYet2 = new bool[3];
 
     // Start is called before the first frame update
     void Start()
     {
+        // Initialize all prefabs as not spawned yet
+        for (int i = 0; i <Prefabs.Length; i++)
+        {
+            spawnedYet2[i] = false;
+        }
+
+        // start spawning things and changing their status accordingly
+        for (int i = 0; i < Prefabs.Length; i++)
+        {
+            while (spawnedYet2[i] == false)
+            {
+                SpawnEnemyNPC(i);
+            }
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SpawnEnemyNPC(int i)
     {
-        
+        Vector2 randPosition = new Vector2(Random.Range(-30.0f, 8.8f), Random.Range(-19.0f, 6.0f));
+        GameObject npc = Instantiate(Prefabs[i]);
+        npc.transform.position = randPosition;
+
+        if (!Physics2D.OverlapCircle(npc.transform.position, 2f))
+        {
+            Debug.Log("Nothing Touches b");
+            spawnedYet2[i] = true;
+        }
+        else if (Physics2D.OverlapCircle(npc.transform.position, 2f))
+        {
+            Debug.Log("Something touches");
+            Destroy(npc);
+        }
     }
 }
